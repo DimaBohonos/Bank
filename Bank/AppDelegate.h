3e21309a74a7c3bcc1f56089940ce046fc5774f9
MenDu4 <mendu4@macbook-air.local>
2017-04-26 16:24:43 +0300

    Initial Commit

---
+++ Bank/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bank
+//
+//  Created by MenDu4 on 26.04.17.
+//  Copyright © 2017 MenDu4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bank
+//
+//  Created by MenDu4 on 26.04.17.
+//  Copyright © 2017 MenDu4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
